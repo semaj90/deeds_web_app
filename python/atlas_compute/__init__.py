3a2325e26ba6f3c9f657509dfae1e4f3c23aad3f
James Woodard <j.ames90@hotmail.com>
2026-08-18 21:54:49 -0700

    feat(atlas): export graph spectral representation and neural routing compute modules

--- python/atlas_compute/__init__.py
+++ python/atlas_compute/__init__.py
@@ -8,9 +8,11 @@ from .ann_compare import AnnComparisonReceipt, compare_cuvs_exact_and_cagra
 from .cugraph_ppr import CuGraphPprParityReceipt, run_cugraph_ppr_parity
 from .determinism import TorchDeterminismReceipt, configure_torch_determinism
 from .exact_semantic import ExactSemanticSearchReceipt, exact_semantic_search
+from .graph_programs import BfsReceipt, CondensationDagReceipt, deterministic_bfs, condense_and_lexicographically_sort
 from .hypergraph_tensor import HypergraphTensorPprReceipt, run_tensor_ppr
 from .interpolation import TensorInterpolationReceipt, interpolate_topology_field
 from .low_rank import LowRankComparisonReceipt, compare_low_rank_recommendations
+from .neural_router import NeuralRouterReceipt, train_receipt_router
 from .rapids_matrix import (
     RapidsKMeansReceipt,
     RapidsPcaReceipt,
@@ -18,6 +20,8 @@ from .rapids_matrix import (
     run_cuvs_kmeans,
     run_cuvs_pca,
 )
+from .representation_compare import RepresentationComparisonReceipt, compare_representations
+from .spectral import SpectralReceipt, symmetric_eigenspace
 
 __all__ = [
     "AnnComparisonReceipt",
@@ -28,15 +32,25 @@ __all__ = [
     "configure_torch_determinism",
     "ExactSemanticSearchReceipt",
     "exact_semantic_search",
+    "BfsReceipt",
+    "CondensationDagReceipt",
+    "deterministic_bfs",
+    "condense_and_lexicographically_sort",
     "HypergraphTensorPprReceipt",
     "run_tensor_ppr",
     "TensorInterpolationReceipt",
     "interpolate_topology_field",
     "LowRankComparisonReceipt",
     "compare_low_rank_recommendations",
+    "NeuralRouterReceipt",
+    "train_receipt_router",
     "RapidsKMeansReceipt",
     "RapidsPcaReceipt",
     "deterministic_farthest_first_ordinals",
     "run_cuvs_kmeans",
     "run_cuvs_pca",
+    "RepresentationComparisonReceipt",
+    "compare_representations",
+    "SpectralReceipt",
+    "symmetric_eigenspace",
 ]
