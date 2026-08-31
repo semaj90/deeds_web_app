b8e76a20e0520eef14256df3e0d06dc2dcc9696d
James Woodard <j.ames90@hotmail.com>
2026-08-31 10:59:24 -0700

    feat(oak): add public OaK 2026 DSPy TS entrypoint

---
+++ packages/parent-atlas/src/oak2026-dspy.ts
@@ -0,0 +1,5 @@
+export * from './core/oak2026-dspy-policy-v1.js';
+export * from './core/kernel-bound-dag-planner-v1.js';
+export * from './core/ontology-kernel-manifest-v1.js';
+export * from './core/kernel-function-catalog-v1.js';
+export * from './core/kernel-operator-library-v1.js';
